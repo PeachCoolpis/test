efb16d5fc4737e877e07a09edcfbe47f59b0d69f
PeachCoolpis <rlglxh@naver.com>
2024-07-20 15:32:45 +0900

    test3

--- src/main/java/com/example/testshop/Home.java
+++ src/main/java/com/example/testshop/Home.java
@@ -1,7 +1,6 @@
 package com.example.testshop;
 
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +9,6 @@ public class Home {
     
     @GetMapping("/")
     public String home() {
-        return "e4i2 도메인";
+        return "M연시";
     }
 }
